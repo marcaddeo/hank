33ec2eaa1e1a890a07976f9bc05dc82437095fe4
Marc Addeo <marcaddeo@gmail.com>
2023-10-07 19:37:50 -0400

    Implement basic Discord bot scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error, sync::Arc};
+use twilight_cache_inmemory::{InMemoryCache, ResourceType};
+use twilight_gateway::{Event, Shard, ShardId};
+use twilight_http::Client as HttpClient;
+use twilight_model::gateway::Intents;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Initialize the tracing subscriber.
+    tracing_subscriber::fmt::init();
+
+    let token = env::var("DISCORD_TOKEN")?;
+
+    // Use intents to only receive guild message events.
+    let mut shard = Shard::new(
+        ShardId::ONE,
+        token.clone(),
+        Intents::GUILD_MESSAGES | Intents::MESSAGE_CONTENT,
+    );
+
+    // HTTP is separate from the gateway, so create a new client.
+    let http = Arc::new(HttpClient::new(token));
+
+    // Since we only care about new messages, make the cache only
+    // cache new messages.
+    let cache = InMemoryCache::builder()
+        .resource_types(ResourceType::MESSAGE)
+        .build();
+
+    // Process each event as they come in.
+    loop {
+        let event = match shard.next_event().await {
+            Ok(event) => event,
+            Err(source) => {
+                tracing::warn!(?source, "error receiving event");
+
+                if source.is_fatal() {
+                    break;
+                }
+
+                continue;
+            }
+        };
+
+        // Update the cache with the event.
+        cache.update(&event);
+
+        tokio::spawn(handle_event(event, Arc::clone(&http)));
+    }
+
+    Ok(())
 }
+
+async fn handle_event(
+    event: Event,
+    http: Arc<HttpClient>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match event {
+        Event::MessageCreate(msg) if msg.content == "!ping" => {
+            http.create_message(msg.channel_id)
+                .content("Pong!")?
+                .await?;
+        }
+        // Other events here...
+        _ => {}
+    }
+
+    Ok(())
+}
+
