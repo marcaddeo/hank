90818db1d2e46d97665301cbfa045c427268e9ae
Marc Addeo <marcaddeo@gmail.com>
2023-11-22 10:41:45 -0500

    Add Wordle plugin

---
+++ plugin/wordle/src/lib.rs
@@ -0,0 +1,51 @@
+use extism_pdk::*;
+use hank_transport::{HankEvent, Message, SubscribedEvents};
+use serde::{Deserialize, Serialize};
+
+#[host_fn]
+extern "ExtismHost" {
+    pub fn send_message(message: Json<Message>);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum PluginResult {
+    Init(SubscribedEvents),
+    HandleEventResult,
+}
+
+#[plugin_fn]
+pub fn handle_event(Json(event): Json<HankEvent>) -> FnResult<Json<PluginResult>> {
+    if event.name == "MessageCreate" {
+        let payload: Message = serde_json::from_str(&event.payload).unwrap();
+
+        if payload.content == "!wordle" {
+            let message = Message {
+                channel_id: payload.channel_id,
+                content: "Wordle!".into(),
+            };
+            unsafe {
+                let _ = send_message(Json(message));
+            }
+        }
+    }
+
+    Ok(Json(PluginResult::HandleEventResult))
+}
+
+#[plugin_fn]
+pub fn init() -> FnResult<Json<PluginResult>> {
+    Ok(Json(PluginResult::Init(SubscribedEvents(vec![
+        "MessageCreate".into(),
+    ]))))
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
